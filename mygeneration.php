03893d5ee9187d23192c48cac6b291c718fcf7a5
Justin Freeman <justin@agileware.com.au>
2024-01-25 11:56:20 +1100

    CIVICRM-2204 civix upgrade

--- mygeneration.php
+++ mygeneration.php
@@ -15,15 +15,6 @@ function mygeneration_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function mygeneration_civicrm_xmlMenu(&$files) {
-  _mygeneration_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -33,48 +24,12 @@ function mygeneration_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function mygeneration_civicrm_postInstall() {
-  _mygeneration_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function mygeneration_civicrm_uninstall() {
-  _mygeneration_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function mygeneration_civicrm_enable() {
   _mygeneration_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function mygeneration_civicrm_disable() {
-  _mygeneration_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function mygeneration_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _mygeneration_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
@@ -100,60 +55,6 @@ function mygeneration_civicrm_managed(&$entities) {
       'run_frequency' => 'Daily',
     ],
   ];
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function mygeneration_civicrm_caseTypes(&$caseTypes) {
-  _mygeneration_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function mygeneration_civicrm_angularModules(&$angularModules) {
-  _mygeneration_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function mygeneration_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _mygeneration_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function mygeneration_civicrm_entityTypes(&$entityTypes) {
-  _mygeneration_civix_civicrm_entityTypes($entityTypes);
-}
-
-/**
- * Implements hook_civicrm_themes().
- */
-function mygeneration_civicrm_themes(&$themes) {
-  _mygeneration_civix_civicrm_themes($themes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
